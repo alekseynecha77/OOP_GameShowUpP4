73764557fba3cf9276288bdf33c0a3a9bfc75a08
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-10 15:49:54 -0500

    update

--- js/Phrase.js
+++ js/Phrase.js
@@ -25,18 +25,6 @@ addPhraseToDisplay() {
         }
   }
 }
-/**
-* Checks if passed letter is in phrase
-* @param (string) letter - Letter to check
-*/
-// checkLetter(letter) {};
-
-/**
-* Displays passed letter on screen after a match is found
-* @param (string) letter - Letter to display
-*/
-// showMatchedLetter(letter) {};
 }
 
-//(this.phrase[i] >= "A" && this.phrase[i] <= "Z") || (this.phrase[i] >= "a" && this.phrase[i] <= "z")
 
